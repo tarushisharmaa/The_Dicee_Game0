299d65796d65b834c4e85d8b03f97152e2aa1cea
tarushisharmaa <85124418+tarushisharmaa@users.noreply.github.com>
2023-07-27 20:53:26 +0530

    1st upload

---
+++ dicess.js
@@ -0,0 +1,19 @@
+var rn1= Math.floor( Math.random()*6)+1;
+var rdi = "dice" + rn1 + ".png";
+var ris = "images/" + rdi;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",ris);
+
+var rn2= Math.floor(Math.random()*6)+1;
+var ris2= "images/dice"+ rn2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",ris2);
+
+if(rn1> rn2){
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
+}
+else if(rn2> rn1){
+    document.querySelector("h1").innerHTML="🚩Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
